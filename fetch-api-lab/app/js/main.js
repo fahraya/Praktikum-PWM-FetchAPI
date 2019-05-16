deea0e9a618f4ecd9bc0c1a73d9f45c89589e570
Fahra Yanu Ramadhani <50445016+fahraya@users.noreply.github.com>
2019-05-16 16:59:44 +0700

    Add files via upload

---
+++ fetch-api-lab/app/js/main.js
@@ -0,0 +1,96 @@
+/*
+Copyright 2018 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// helper functions ----------
+
+function logResult(result) {
+  console.log(result);
+}
+
+function logError(error) {
+  console.log('Looks like there was a problem:', error);
+}
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+function readResponseAsJSON(response) {
+  return response.json();
+}
+
+function readResponseAsDATA(response) {
+  return response.json();
+}
+
+// Fetch JSON ----------
+
+function fetchJSON() { //fungsi untuk fetchJSON
+	fetch('examples/animals.json') //mengambil file json
+	.then(validateResponse)	
+	.then(readResponseAsJSON)
+	.then(logResult)	
+	.catch(logError);
+}
+const jsonButton = document.getElementById('json-btn');
+jsonButton.addEventListener('click', fetchJSON);
+
+function fetchDATA() {
+	fetch('examples/data.json')
+	.then(validateResponse)	
+	.then(readResponseAsDATA)
+	.then(logResult)	
+	.catch(logError);
+}
+const dataButton = document.getElementById('data-btn');
+dataButton.addEventListener('click', fetchDATA);
+// Fetch Image ----------
+
+function fetchImage() {
+  // TODO
+}
+const imgButton = document.getElementById('img-btn');
+imgButton.addEventListener('click', fetchImage);
+
+
+// Fetch text ----------
+
+function fetchText() {
+  // TODO
+}
+const textButton = document.getElementById('text-btn');
+textButton.addEventListener('click', fetchText);
+
+
+// HEAD request ----------
+
+function headRequest() {
+  // TODO
+}
+const headButton = document.getElementById('head-btn');
+headButton.addEventListener('click', headRequest);
+
+
+// POST request ----------
+
+/* NOTE: Never send unencrypted user credentials in production! */
+function postRequest() {
+  // TODO
+}
+const postButton = document.getElementById('post-btn');
+postButton.addEventListener('click', postRequest);
